28ad1ea4ee1def61f5038683a15b55595464663b
Harry Newman <h.newman2005@outlook.com>
2025-08-16 21:03:22 +0100

    Changed version definitions to an enum, made clang-tidy only run on my own header files (it genuinely would not stop complaining)

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,20 @@
 
 #include <GLFW/glfw3.h>
 
-#define OPEN_GL_VERSION_MAJOR 4
-#define OPEN_GL_VERSION_MINOR 6
+#include <cstdint>
+
+enum class Version : uint8_t {
+  OPEN_GL_VERSION_MAJOR = 4,
+  OPEN_GL_VERSION_MINOR = 8
+};
 
 int main() {
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, OPEN_GL_VERSION_MAJOR);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, OPEN_GL_VERSION_MINOR);
+  if (glfwInit() == 0) {
+    return -1;
+  };
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,
+                 static_cast<int>(Version::OPEN_GL_VERSION_MAJOR));
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,
+                 static_cast<int>(Version::OPEN_GL_VERSION_MINOR));
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
