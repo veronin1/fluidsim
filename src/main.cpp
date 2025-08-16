b30819721705f73158660b892a79efbc939a8d46
Harry Newman <h.newman2005@outlook.com>
2025-08-16 21:14:46 +0100

    Create a class for WindowConfig (width, height, title) create window and null check and then make the context of the window the main context on the current thread

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,23 @@ enum class Version : uint8_t {
   OPEN_GL_VERSION_MINOR = 8
 };
 
+class WindowConfig {
+ public:
+  static constexpr int DEFAULT_WIDTH = 800;
+  static constexpr int DEFAULT_HEIGHT = 600;
+  static constexpr const char *DEFAULT_TITLE = "fluidsim";
+
+  int width;
+  int height;
+  const char *title;
+
+  WindowConfig()
+      : width(DEFAULT_WIDTH), height(DEFAULT_HEIGHT), title(DEFAULT_TITLE) {}
+
+  WindowConfig(int winWidth, int winHeight, const char *winTitle)
+      : width(winWidth), height(winHeight), title(winTitle) {}
+};
+
 int main() {
   if (glfwInit() == 0) {
     return -1;
@@ -18,4 +35,14 @@ int main() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,
                  static_cast<int>(Version::OPEN_GL_VERSION_MINOR));
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  const WindowConfig config;
+
+  GLFWwindow *window = glfwCreateWindow(config.width, config.height,
+                                        config.title, nullptr, nullptr);
+  if (window == nullptr) {
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
 }
