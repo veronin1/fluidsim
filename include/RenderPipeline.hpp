883f2b3cd7dfd21cff8c500bff7a2374b5e66bdc
Harry Newman <h.newman2005@outlook.com>
2025-08-19 13:57:47 +0100

    Added RenderPipeline class & definitions, created a variable called vertexCount so its not hardstuck at X amount of vertices and included the shaderSources in the definitions of the shader creations (frag & vertex)

--- include/RenderPipeline.hpp
+++ include/RenderPipeline.hpp
@@ -3,6 +3,9 @@
 #include <glad/glad.h>
 
 #include <array>
+#include <cstdlib>
+
+constexpr size_t SHADER_INFO_LOG_SIZE = 512;
 
 class RenderPipeline {
  public:
@@ -10,7 +13,9 @@ class RenderPipeline {
   RenderPipeline(RenderPipeline &&) = delete;
   RenderPipeline &operator=(const RenderPipeline &) = default;
   RenderPipeline &operator=(RenderPipeline &&) = delete;
-  explicit RenderPipeline(const std::array<float, 9> &vertices);
+  explicit RenderPipeline(const std::array<float, 9> &vertices,
+                          const char *vertexShaderSource,
+                          const char *fragmentShaderSource);
   ~RenderPipeline();
 
   void draw() const;
@@ -19,9 +24,10 @@ class RenderPipeline {
   GLuint VAO{};
   GLuint VBO{};
   GLuint shaderProgram;
+  GLsizei vertexCount{};
 
-  static GLuint createVertexShader();
-  static GLuint createFragmentShader();
+  static GLuint createVertexShader(const char *vertexShaderSource);
+  static GLuint createFragmentShader(const char *fragmentShaderSource);
   static GLuint createShaderProgram(GLuint vertexShader, GLuint fragmentShader);
-  static void linkVertexAttributes();
+  void linkVertexAttributes();
 };
