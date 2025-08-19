2447b16f5758a47e0bfdb92385dda3e43514006d
Harry Newman <h.newman2005@outlook.com>
2025-08-19 18:57:28 +0100

    Advection progress (hell)

--- src/navier.cpp
+++ src/navier.cpp
@@ -48,4 +48,27 @@ void applyForces(float timeStep, Liquid& fluid) {
   }
 }
 
-// advection(Grid3D& grid, float timeStep,
+void advection(Grid3D& grid, Liquid& fluid, float timeStep) {
+  const Liquid& fluidPrev = fluid;
+  constexpr float CELL_CENTER_OFFSET = 0.5F;
+
+  for (size_t z = 0; z < grid.nz; ++z) {
+    for (size_t y = 0; y < grid.ny; ++y) {
+      for (size_t x = 0; x < grid.nx; ++x) {
+        size_t index = grid.idx(x, y, z);
+        Vec3 currentCell = {static_cast<float>(x) + CELL_CENTER_OFFSET,
+                            static_cast<float>(y) + CELL_CENTER_OFFSET,
+                            static_cast<float>(z) + CELL_CENTER_OFFSET};
+
+        Vec3 prev = {currentCell.x - (fluidPrev.velocity[index].x * timeStep),
+                     currentCell.y - (fluidPrev.velocity[index].y * timeStep),
+                     currentCell.z - (fluidPrev.velocity[index].z * timeStep)};
+
+        Vec3 interpolatedVelocity = {prev.x - };
+      }
+    }
+  }
+}
+
+float trilinearInterpolate(const Grid3D& grid, const std::vector<Vec3>& field,
+                           const Vec3& pos) {}
