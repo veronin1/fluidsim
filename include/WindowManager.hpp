019ae136ed72d0235d83df92d7e06c0302aaa8ad
Harry Newman <h.newman2005@outlook.com>
2025-08-19 13:20:49 +0100

    Moved redundant code ot their own files

--- include/WindowManager.hpp
+++ include/WindowManager.hpp
@@ -2,26 +2,7 @@
 
 #include <GLFW/glfw3.h>
 
-#include <cstdint>
-
-enum class OpenGLVersion : uint8_t { VERSION_MAJOR = 4, VERSION_MINOR = 6 };
-
-class WindowConfig {
- public:
-  static constexpr int DEFAULT_WIDTH = 800;
-  static constexpr int DEFAULT_HEIGHT = 600;
-  static constexpr const char *DEFAULT_TITLE = "fluidsim";
-
-  int width;
-  int height;
-  const char *title;
-
-  WindowConfig()
-      : width(DEFAULT_WIDTH), height(DEFAULT_HEIGHT), title(DEFAULT_TITLE) {}
-
-  WindowConfig(int winWidth, int winHeight, const char *winTitle)
-      : width(winWidth), height(winHeight), title(winTitle) {}
-};
+class WindowConfig;
 
 class WindowManager {
  public:
