8c9394d255d91cab16ccc2170f91fecba7e12ca0
Harry Newman <h.newman2005@outlook.com>
2025-08-19 13:06:39 +0100

    Created WindowConfig.hpp file

---
+++ include/WindowConfig.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+class WindowConfig {
+ public:
+  static constexpr int DEFAULT_WIDTH = 800;
+  static constexpr int DEFAULT_HEIGHT = 600;
+  static constexpr const char *DEFAULT_TITLE = "fluidsim";
+
+  int width;
+  int height;
+  const char *title;
+
+  WindowConfig()
+      : width(DEFAULT_WIDTH), height(DEFAULT_HEIGHT), title(DEFAULT_TITLE) {}
+
+  WindowConfig(int winWidth, int winHeight, const char *winTitle)
+      : width(winWidth), height(winHeight), title(winTitle) {}
+};
