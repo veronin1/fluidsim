e354f4becba8f17770efa84041be042ff9802207
Harry Newman <h.newman2005@outlook.com>
2025-08-19 16:26:49 +0100

    Created a struct for the grid that has its own index finder function and renamed numXYZ to gridSizeXYZ for more verbosity

--- src/navier.cpp
+++ src/navier.cpp
@@ -2,24 +2,36 @@
 #include <cstddef>
 #include <vector>
 
-constexpr int numX = 100;
-constexpr int numY = 100;
-constexpr int numZ = 50;
+constexpr int gridSizeX = 100;
+constexpr int gridSizeY = 100;
+constexpr int gridSizeZ = 50;
 
 constexpr float DENSITY_WATER_KG_PER_M3 = 997.0F;
 constexpr float GRAVITY_FORCE_EARTH_M_PER_S2 = 9.807F;
 
-inline int idx(int x, int y, int z) { return x + (numX * (numY * z + y)); }
+struct Grid3D {
+  int nx, ny, nz;
+  Grid3D(int x, int y, int z) : nx(x), ny(y), nz(z) {}
+
+  [[nodiscard]] int idx(int x, int y, int z) const;
+};
+
+int Grid3D::idx(int x, int y, int z) const { return x + (nx * (y + ny * z)); }
 
 struct Vec3 {
   float x, y, z;
 };
 
-std::vector<Vec3> velocity(static_cast<size_t>(numX* numY* numZ));
-std::vector<float> density(static_cast<size_t>(numX* numY* numZ));
-std::vector<float> pressure(static_cast<size_t>(numX* numY* numZ));
+std::vector<Vec3> velocity(
+    static_cast<size_t>(gridSizeX* gridSizeY* gridSizeZ));
+std::vector<float> density(
+    static_cast<size_t>(gridSizeX* gridSizeY* gridSizeZ));
+std::vector<float> pressure(
+    static_cast<size_t>(gridSizeX* gridSizeY* gridSizeZ));
 
 int navier() {
+  Grid3D grid(gridSizeX, gridSizeY, gridSizeZ);
+
   // set density to water density (non-changing)
   std::fill(density.begin(), density.end(), DENSITY_WATER_KG_PER_M3);
 
