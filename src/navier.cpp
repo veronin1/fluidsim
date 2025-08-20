e740a722c440ae827b0d07aa4b24c98c28395f85
Harry Newman <h.newman2005@outlook.com>
2025-08-20 20:11:53 +0100

    Added advectDensity and made other functions typename T so they are more general

--- src/navier.cpp
+++ src/navier.cpp
@@ -58,8 +58,33 @@ void advectVelocity(Grid3D& grid, Liquid& fluid, float timeStep) {
   }
 }
 
-Vec3 trilinearInterpolate(const Grid3D& grid, const std::vector<Vec3>& field,
-                          const Vec3& pos) {
+void advectDensity(Grid3D& grid, Liquid& fluid, float timeStep) {
+  const Liquid& fluidPrev = fluid;
+  constexpr float CELL_CENTER_OFFSET = 0.5F;
+
+  for (size_t z = 0; z < grid.nz; ++z) {
+    for (size_t y = 0; y < grid.ny; ++y) {
+      for (size_t x = 0; x < grid.nx; ++x) {
+        size_t index = grid.idx(x, y, z);
+        Vec3 currentCell = {static_cast<float>(x) + CELL_CENTER_OFFSET,
+                            static_cast<float>(y) + CELL_CENTER_OFFSET,
+                            static_cast<float>(z) + CELL_CENTER_OFFSET};
+
+        Vec3 prevPos = {
+            currentCell.x - (fluidPrev.velocity[index].x * timeStep),
+            currentCell.y - (fluidPrev.velocity[index].y * timeStep),
+            currentCell.z - (fluidPrev.velocity[index].z * timeStep)};
+
+        fluid.density[index] =
+            trilinearInterpolate(grid, fluidPrev.density, prevPos);
+      }
+    }
+  }
+}
+
+template <typename T>
+T trilinearInterpolate(const Grid3D& grid, const std::vector<T>& field,
+                       const Vec3& pos) {
   int x0 = -1;
   int y0 = -1;
   int z0 = -1;
@@ -75,29 +100,30 @@ Vec3 trilinearInterpolate(const Grid3D& grid, const std::vector<Vec3>& field,
   float v = pos.y - y0;
   float w = pos.z - z0;
 
-  Vec3 f000 = field[grid.idx(x0, y0, z0)];
-  Vec3 f100 = field[grid.idx(x1, y0, z0)];
-  Vec3 f010 = field[grid.idx(x0, y1, z0)];
-  Vec3 f110 = field[grid.idx(x1, y1, z0)];
-  Vec3 f001 = field[grid.idx(x0, y0, z1)];
-  Vec3 f101 = field[grid.idx(x1, y0, z1)];
-  Vec3 f011 = field[grid.idx(x0, y1, z1)];
-  Vec3 f111 = field[grid.idx(x1, y1, z1)];
+  T f000 = field[grid.idx(x0, y0, z0)];
+  T f100 = field[grid.idx(x1, y0, z0)];
+  T f010 = field[grid.idx(x0, y1, z0)];
+  T f110 = field[grid.idx(x1, y1, z0)];
+  T f001 = field[grid.idx(x0, y0, z1)];
+  T f101 = field[grid.idx(x1, y0, z1)];
+  T f011 = field[grid.idx(x0, y1, z1)];
+  T f111 = field[grid.idx(x1, y1, z1)];
 
   // interpolate along X
-  Vec3 f00 = linearInterpolate(f000, f100, u);
-  Vec3 f10 = linearInterpolate(f010, f110, u);
-  Vec3 f01 = linearInterpolate(f001, f101, u);
-  Vec3 f11 = linearInterpolate(f011, f111, u);
+  T f00 = linearInterpolate(f000, f100, u);
+  T f10 = linearInterpolate(f010, f110, u);
+  T f01 = linearInterpolate(f001, f101, u);
+  T f11 = linearInterpolate(f011, f111, u);
 
   // interpolate along Y
-  Vec3 f0 = linearInterpolate(f00, f10, v);
-  Vec3 f1 = linearInterpolate(f01, f11, v);
+  T f0 = linearInterpolate(f00, f10, v);
+  T f1 = linearInterpolate(f01, f11, v);
 
   // interpolate along Z
   return linearInterpolate(f0, f1, w);
 }
 
-Vec3 linearInterpolate(const Vec3& a, const Vec3& b, float t) {
+template <typename T>
+T linearInterpolate(const T& a, const T& b, float t) {
   return a * (1 - t) + b * t;
 }
