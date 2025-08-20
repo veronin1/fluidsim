c9528bcb49bb86c6581568691c1e22c000e1f623
Harry Newman <h.newman2005@outlook.com>
2025-08-20 20:19:22 +0100

    Created operator functions for the vec so that it can be interpolated properly woo woo

--- include/navier.hpp
+++ include/navier.hpp
@@ -13,6 +13,16 @@ struct Grid3D {
 
 struct Vec3 {
   float x = 0.0F, y = 0.0F, z = 0.0F;
+
+  // vec3 + vec3
+  Vec3 operator+(const Vec3& other) const {
+    return {x + other.x, y + other.y, z + other.z};
+  }
+
+  // vec3 * vec3
+  Vec3 operator*(const Vec3& other) const {
+    return {x * other.x, y * other.y, z * other.y};
+  }
 };
 
 struct Liquid {
