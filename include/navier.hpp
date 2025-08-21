75858fbc55cd73c79d981c1a960f5df9b724cf97
Harry Newman <h.newman2005@outlook.com>
2025-08-21 21:36:14 +0100

    Used coefficient instead of diffusionRate (slightly cleaner)

--- include/navier.hpp
+++ include/navier.hpp
@@ -31,7 +31,7 @@ void advect(Grid3D& grid, const std::vector<T>& velocityField,
 
 template <typename T>
 void diffuse(Grid3D& grid, std::vector<T>& data, std::vector<T>& temp,
-             float diffusionRate, float timeStep) {
+             float coefficient, float timeStep) {
   constexpr float NUM_OF_NEIGHBOURS = 6.0F;
   constexpr size_t MAX_ITERATIONS = 20;
 
@@ -62,7 +62,7 @@ void diffuse(Grid3D& grid, std::vector<T>& data, std::vector<T>& temp,
           T laplacian = neighbourSum - NUM_OF_NEIGHBOURS * (*src)[index];
 
           // diffuse velocity
-          (*dst)[index] = (*src)[index] + diffusionRate * timeStep * laplacian;
+          (*dst)[index] = (*src)[index] + coefficient * timeStep * laplacian;
         }
       }
     }
