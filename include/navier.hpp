a317867553161f45daa34995c1c84ac4ec67cf26
Harry Newman <h.newman2005@outlook.com>
2025-08-21 16:25:11 +0100

    Added diffuseVelocity header

--- include/navier.hpp
+++ include/navier.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
 #include "liquid.hpp"
-#include "vec3.hpp"
 #include "vector_math.hpp"
 
 int navier();
 void applyForces(float timeStep, Liquid& fluid);
 void advectVelocity(Grid3D& grid, Liquid& fluid, float timeStep);
 void advectDensity(Grid3D& grid, Liquid& fluid, float timeStep);
+void diffuseVelocity(Grid3D& grid, Liquid& fluid, float timeStep);
